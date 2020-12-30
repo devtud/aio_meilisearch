f70e6b48f63962595f854974930aa680141a471c
devtud <devtud@gmail.com>
2020-12-30 23:20:10 +0200

    add py 3.7

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("poetry", "run", "python", "-m", "unittest")
