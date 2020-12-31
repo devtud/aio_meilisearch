76c54fa7dc376204cb0629e4af441925670df6f6
devtud <devtud@gmail.com>
2020-12-31 14:54:20 +0200

    install nox-poetry and remove py 3.7

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
 import nox
+import nox_poetry.patch  # noqa
+from nox.sessions import Session
 
 
-@nox.session(python=["3.7", "3.8", "3.9"])
-def tests(session):
-    session.run("poetry", "install", external=True)
+@nox.session(python=["3.8", "3.9"])
+def tests(session: Session):
+    session.install(".")
+    # session.run("poetry", "install", external=True)
     session.run("poetry", "run", "python", "-m", "unittest")
