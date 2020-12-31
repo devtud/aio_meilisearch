8a93d86495a2b644612f972f8394fa5fbb2288de
devtud <devtud@gmail.com>
2020-12-31 16:37:16 +0200

    debug noxfile further

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ from nox.sessions import Session
 @nox.session(python=["3.8", "3.9"])
 def tests(session: Session) -> None:
     session.run("poetry", "lock")
+    session.run("cat", ".nox/tests-3-8/tmp/requirements.txt")
     session.install(".")
     session.install("docker")
-    session.run("cat", ".nox/tests-3-8/tmp/requirements.txt")
     session.run("python", "-m", "unittest")
