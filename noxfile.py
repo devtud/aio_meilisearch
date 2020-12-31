deceb9b9e00989f5077fe41f958bfa130c0dbc2c
devtud <devtud@gmail.com>
2020-12-31 16:06:09 +0200

    poetry lock

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,7 @@ from nox.sessions import Session
 
 @nox.session(python=["3.8", "3.9"])
 def tests(session: Session):
+    session.run("poetry", "lock")
     session.install(".")
     session.install("docker")
     session.run("python", "-m", "unittest")
